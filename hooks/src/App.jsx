77dca270ad2ec292790dad28fc70fdfe3dd7d497
André <andrecarrijo0710@gmail.com>
2025-10-02 10:56:42 -0300

    Criação do Projeto 'hooks'

---
+++ hooks/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+/*sou uma cobrinha andando na tela e deixando rastro*/
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
