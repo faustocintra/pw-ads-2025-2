22141e84409351449eb17c95bc3b56ed919ee0bd
JJunioor <jalisson.brito@fatec.sp.gov.br>
2025-10-03 09:07:46 -0300

    (03/10) React Hooks: exercício 01

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import './App.css'
 
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
+
+import Home from './Home'
+import Exercicio01 from './exercicios/01.jsx'
+import Exercicio02 from './exercicios/02.jsx'
 function App() {
 
   return (
     <>
-      
+      <h1>Exercícios React Hooks</h1>
+      <BrowserRouter>
+        <ul>
+          <li> <Link to="/">Página inicial</Link> </li>
+        </ul>
+
+        <ul>
+          <li> <Link to="/01">Exercício 01</Link> </li>
+        </ul>
+
+        <ul>
+          <li> <Link to="/01">Exercício 02</Link> </li>
+        </ul>
+        
+        <hr />
+
+        <Routes>
+          {/* path="/" significa a raiz do website */}
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+        <Routes>
+        <Route path="/01" element={<Exercicio01 />} />
+        </Routes>
+
+        <Routes>
+        <Route path="/02" element={<Exercicio02 />} />
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
+
+export default App
--- hooks/src/exercicios/01.jsx
+++ hooks/src/exercicios/01.jsx
@@ -2,16 +2,23 @@ import * as React from 'react'
 
 function Greeting({initialName}) {
   // 💣 exclua esta declaração de variável e troque por uma chamada a React.useState
-  const name = ''
+    const [name, setname ] = React.useState(initialName); //Valor inicial
 
   function handleChange(event) {
     // 🐨 atualize o nome aqui baseado em event.target.value
+    setname(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Nome: </label>
+        {/*Em REACT, um COMPONENTE CONTROLADO é aquele que tem seu valor 
+        sincronizado com uma variável de estado. Assim, quando a variável de estado muda 
+        o seu valor, o valor do componente também muda. O inverso também ocorre: se o valor do
+        componente muda, o valor da variável de estado também é alterado 
+        No caso abaixo, isso é implementado associando o valor do atributo
+        "value" á variável de estado.*/}
         <input onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Olá {name}</strong> : 'Por favor, informe seu nome'}
@@ -19,8 +26,8 @@ function Greeting({initialName}) {
   )
 }
 
-function App() {
+function Exercicio01() {
   return Greeting({initialName: "Turíbio"})
 }
 
-export default App
+export default Exercicio01
