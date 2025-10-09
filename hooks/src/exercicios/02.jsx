8af3a76c22b34d691594cc91b2d565b8e3cfa10a
dfadomingos <dfadomingos47@gmail.com>
2025-10-09 10:54:53 -0300

    (09/10) React Hooks: exercício 02

--- hooks/src/exercicios/02.jsx
+++ hooks/src/exercicios/02.jsx
@@ -3,11 +3,33 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 inicialize o estado como o valor do localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+
+  //verifica se existe algo salvo no local storage com a chave 'name'
+  //Caso exista, usa esse valor. Se não existir, usa o valor de initialName
+  const [name, setName] = React.useState(
+
+    // Lazy initializer => quando o valor inicial de uma variável de estado
+    // é FORNECIDO POR UMA FUNÇÃO, garantimos que esse valor inicial seja
+    // atribuido apenas uma vez, na fase de montagem do componente
+    () => window.localStorage.getItem('name') ?? initialName
+  )
+
+  // function inicializarVar(){
+  //  console.count('Inicializou "name"!')
+  //  return window.localStorage.getItem('name') ?? initialName
+  // }
+
+  const [count, setCount] = React.useState(0)
 
   // 🐨 Aqui é onde usamos `React.useEffect`.
   // A função deve armazenar `name` no localStorage.
   // 💰 window.localStorage.setItem('name', name)
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+    console.count('Componente foi atualizado!')
+  }, [name]) //vetor de dependencias
+    // este useEffect
 
   function handleChange(event) {
     setName(event.target.value)
@@ -19,12 +41,15 @@ function Greeting({initialName = ''}) {
         <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Olá {name}</strong> : 'Por favor, informe seu nome'}
+      <br /><button onClick={() => setCount(count + 1)}>
+        Contagem: {count}
+      </button>
     </div>
   )
 }
 
 function Exercicio02() {
-  return <Greeting />
+  return <Greeting  initialName='Orozinho'/>
 }
 
 export default Exercicio02
