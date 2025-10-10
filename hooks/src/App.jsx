bed17d36cc4ba35dba7d12f2fcefef3d64ffccbd
dfadomingos <dfadomingos47@gmail.com>
2025-10-10 09:07:42 -0300

    (10/10) React Hooks: exercício 03 (completo) e 04 (parcial)

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Exercicio01 from './exercicios/01.jsx'
 import Exercicio02 from './exercicios/02.jsx'
 import Exercicio03 from './exercicios/03.jsx'
+import Exercicio04 from './exercicios/04.jsx'
 
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
 
@@ -19,6 +20,7 @@ function App() {
           <li> <Link to="/01">Exercício 01</Link> </li>
           <li> <Link to="/02">Exercício 02</Link> </li>
           <li> <Link to="/03">Exercício 03</Link> </li>
+          <li> <Link to="/04">Exercício 04</Link> </li>
         </ul>
         
         <hr />
@@ -29,6 +31,7 @@ function App() {
           <Route path="/01" element={<Exercicio01 />} />
           <Route path="/02" element={<Exercicio02 />} />
           <Route path="/03" element={<Exercicio03 />} />
+          <Route path="/04" element={<Exercicio04 />} />
         </Routes>
       </BrowserRouter>
     </>
