7fe487ace0299d08c0fe6b43c4b1838954d5410a
Hugo Mendes <hugogs.mendes@gmail.com>
2025-10-10 09:09:15 -0300

    (10/10) React Hooks: exercicio3 completo e exercicio3 parcial

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Exercicio01 from './exercicios/01.jsx'
 import Exercicio02 from './exercicios/02.jsx'
 import Exercicio03 from './exercicios/03.jsx'
+import Exercicio04 from './exercicios/04.jsx'
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom'
 import Home from './Home'
 
@@ -17,6 +18,7 @@ function App() {
           <li> <Link to="/01">Exercício 1</Link> </li>
           <li> <Link to="/02">Exercício 2</Link> </li>
           <li> <Link to="/03">Exercício 3</Link> </li>
+          <li> <Link to="/04">Exercício 4</Link> </li>
         </ul>
         
         <hr />
@@ -27,6 +29,7 @@ function App() {
           <Route path="/01" element={<Exercicio01 />} />
           <Route path="/02" element={<Exercicio02 />} />
           <Route path="/03" element={<Exercicio03 />} />
+          <Route path="/04" element={<Exercicio04 />} />
         </Routes>
       </BrowserRouter>
     </>
