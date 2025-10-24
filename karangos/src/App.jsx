5ad46bdd03799061bcb3b915d2755bc97fd49bf8
Ana Laura <analauracampioni@hotmail.com>
2025-10-24 18:52:03 -0300

    (24/10) Instalação da biblioteca MUI e primeiro componente

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -1,11 +1,14 @@
 //import './App.css'
 import { BrowserRouter } from 'react-router-dom'
+import HeaderBar from './ui/HeaderBar'
+import CssBaseline from '@mui/material/CssBaseline'
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <BrowserRouter>
-    
+        <HeaderBar />
       </BrowserRouter>
     </>
   )
