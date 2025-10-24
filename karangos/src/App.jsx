83ba979d19ce286856a28cc373b4a6f3ae64bb65
ANDREA DE FATIMA BORGES LIMA <1090482411004@fatec-srv.local>
2025-10-24 09:02:44 -0300

    (24/10) Instalação da biblioteca MUI e primeiro componente

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 import { BrowserRouter } from 'react-router-dom'
+import HeaderBar from './ui/HeaderBar'
+import CssBaseline from '@mui/material/CssBaseline'
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <BrowserRouter>
-      
+        <HeaderBar />
       </BrowserRouter>
     </>
   )
@@ -16,6 +19,6 @@ function App() {
 
 export default App
 
-//recomendações de design m3.materiao.io do Google para padronizar aplicações Android
+//recomendações de design m3.material.io do Google para padronizar aplicações Android
 //foi trazido para web também e traduziu-se para CSS.
-//juntou com o React e formou-se a biblioteca pronta de design (componentes visuaus): mui.com
+//juntou com o React e formou-se a biblioteca pronta de design (componentes visuais): mui.com
