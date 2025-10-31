c37c5a2a8b5a702d9ddd07d58882569b18397bfd
JJunioor <jalisson.brito@fatec.sp.gov.br>
2025-10-31 09:09:42 -0300

    (31/10) Tema, menu principal e barra de rodapé

--- karangos/src/ui/HeaderBar.jsx
+++ karangos/src/ui/HeaderBar.jsx
@@ -2,29 +2,32 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
- 
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
- 
+
+
+
 import logotipo from '../assets/karangos-logo-600px.png'
- 
+
+
+import MainMenu from './MainMenu'
+
+
 export default function HeaderBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
- 
-          <a href="/">
-           <img src={logotipo} alt="Logotipo Karangos" title="Karangos &bull; Os velhinhos são nossa paixão"
-           style={{ width: '300px' }} />
+ return (
+   <Box sx={{ flexGrow: 1 }}>
+     <AppBar position="static" enableColorOnDark>
+       <Toolbar variant="dense">
+         
+         <MainMenu />
+         <a href="/">
+           <img
+             src={logotipo}
+             alt="Logotipo Karangos"
+             title="Karangos &bull; Os velhinhos são nossa paixão"
+             style={{ width: '300px' }}
+           />
          </a>
-         
-         
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+       </Toolbar>
+     </AppBar>
+   </Box>
+ );
 }
