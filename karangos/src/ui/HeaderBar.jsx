750fb21db36f51e97cff6f6cde73184c9b8df2a5
dfadomingos <dfadomingos47@gmail.com>
2025-10-31 09:11:09 -0300

    (31/10) Tema, menu principal e barra de rodapé

--- karangos/src/ui/HeaderBar.jsx
+++ karangos/src/ui/HeaderBar.jsx
@@ -2,23 +2,15 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-
 import logotipo from '../assets/karangos-logo-600px.png'
+import MainMenu from './MainMenu'
 
 export default function HeaderBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Photos
-          </Typography>
+      <AppBar position="static" enableColorOnDark>        
+        <Toolbar variant="dense">          
+          <MainMenu />
           <a href="/">
            <img 
             src={logotipo} 
