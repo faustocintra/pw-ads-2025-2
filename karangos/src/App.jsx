369baedfcebac37ef66f6877cd45444b2ac960c4
JJunioor <jalisson.brito@fatec.sp.gov.br>
2025-10-31 08:03:00 -0300

    (24/10) Instalação da biblioteca MUI e primeiro componente

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -1,16 +1,19 @@
 // import './App.css'
 import { BrowserRouter } from 'react-router-dom'
-
-
+ 
+import HeaderBar from './ui/HeaderBar'
+import CssBaseline from '@mui/material/CssBaseline'
+ 
 function App() {
  return (
    <>
+    <CssBaseline />
      <BrowserRouter>
-    
+    <HeaderBar />
      </BrowserRouter>
    </>
  )
 }
-
-
+ 
+ 
 export default App
