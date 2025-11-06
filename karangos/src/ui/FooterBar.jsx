d7104390dade0e8d686c513dae836a47b3b1fcad
Ana Laura <analauracampioni@hotmail.com>
2025-11-06 10:57:10 -0300

    (06/11) Término da barra de rodapé

--- karangos/src/ui/FooterBar.jsx
+++ karangos/src/ui/FooterBar.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import CoffeeIcon from '@mui/icons-material/Coffee';
 
 
 export default function FooterBar() {
  return <>
-   <Box
-     component="footer"
-     sx={{
-       backgroundColor: 'action.disabledBackground',
-       display: 'flex',
-       justifyContent: 'center',
-       position: 'fixed',  // posição fixa
-       bottom: 0,          // na parte de baixo da página
-       width: '100vw'
-     }}
-   >
-     Texto de teste
-   </Box>
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: 'action.disabledBackground',
+        display: 'flex',
+        justifyContent: 'center',
+        position: 'fixed',  // posição fixa
+        bottom: 0,          // na parte de baixo da página
+        width: '100vw'
+      }}
+    >
+      <Typography 
+        variant="caption"
+          sx={{
+          '& a': {  // Altera a cor do link (a) dentro do Typography (&)
+            color: 'secondary.light'
+          }
+        }}
+      >
+        Desenvolvido e mantido com <CoffeeIcon fontSize="small" /> por <a href="mailto:analauracampioi@hotmail.com">Ana Laura</a>
+      </Typography>
+    </Box>
  </>
 }
