ef63467d917d759784752177f3adfffad41a53a8
André <andrecarrijo0710@gmail.com>
2025-11-06 10:58:08 -0300

    Aula 06/11 - Término do Footer

--- karangos/src/ui/FooterBar.jsx
+++ karangos/src/ui/FooterBar.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 
+import Typography from '@mui/material/Typography'
+import CoffeeIcon from '@mui/icons-material/Coffee';
+
 
 export default function FooterBar() {
- return <>
-   <Box
-     sx={{
-       backgroundColor: 'action.disabledBackground',
-       display: 'flex',
-       justifyContent: 'center',
-       position: 'fixed',  // posição fixa
-       bottom: 0,          // na parte de baixo da página
-       width: '100vw'
-     }}
-   >
-     &copy; Karangos
-   </Box>
- </>
+  return <>
+    <Box
+      sx={{
+        backgroundColor: 'action.disabledBackground',
+        display: 'flex',
+        justifyContent: 'center',
+        position: 'fixed',  // posição fixa
+        bottom: 0,          // na parte de baixo da página
+        width: '100vw'
+      }}
+    >
+      <Typography
+        variant="caption"
+        sx={{
+          '& a': {  // Altera a cor do link (a) dentro do Typography (&)
+            color: 'secondary.light',
+          }
+        }}
+      >
+        Desenvolvido e mantido com <CoffeeIcon fontSize='small'/> por <a href="mailto:andrecarrijo0710@gmail.com">André Carrijo</a>
+      </Typography>
+    </Box>
+  </>
 }
