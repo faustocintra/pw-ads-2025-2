be648a63a60915779e964e9979a44aac25e21352
Bruna <bruna.fatec102@gmail.com>
2025-11-07 09:04:17 -0300

    (07/11) Navegação SPA com rotas e menus

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -3,7 +3,10 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 import HeaderBar from './ui/HeaderBar'
+
+
 import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
 
 
 import { ThemeProvider } from '@mui/material/styles'
@@ -11,6 +14,9 @@ import theme from './ui/theme'
 
 
 import FooterBar from './ui/FooterBar'
+
+
+import AppRoutes from './routes/AppRoutes'
 
 
 function App() {
@@ -22,6 +28,14 @@ function App() {
       
        <BrowserRouter>
          <HeaderBar />
+
+
+         {/* Dentro da prop "sx", "m" significa "margin" */}
+         <Box id="innerRoot" sx={{ m: '48px 24px' }}>
+           <AppRoutes />
+         </Box>
+
+
          <FooterBar />
        </BrowserRouter>
     
