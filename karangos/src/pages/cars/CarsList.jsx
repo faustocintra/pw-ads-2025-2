717641ae258a77a87b8c1359334cca57b0de2ea5
arthurkitice <art.ferreira301@gmail.com>
2025-11-14 09:19:08 -0300

    Inicio do trabalho 2

--- karangos/src/pages/cars/CarsList.jsx
+++ karangos/src/pages/cars/CarsList.jsx
@@ -1,11 +1,102 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 
+export default function CustomersList() {
 
-export default function CarsList() {
- return <>
-   <Typography variant="h1">
-     Listagem de veículos
-   </Typography>
- </>
+  const columns = [
+    {
+      field: 'id',
+      headerName: 'Cód.',
+      width: 90
+    },
+    {
+      field: 'brandModel',
+      headerName: 'Modelo',
+      width: 250,
+      renderCell: (params, value, row) => {
+        return params.value + " " + row.brand
+      }
+    },
+    {
+      field: 'color',
+      headerName: 'Cor',
+      width: 150
+    },
+    {
+      field: 'year_manufacture',
+      headerName: 'Ano de Fabr.',
+      width: 150
+    },
+    {
+      field: 'imported',
+      headerName: 'Importado',
+      width: 150,
+      renderCell: params => {
+        return params.value === 1 ? "SIM" : ""
+      }
+    },
+    {
+      field: 'plates',
+      headerName: 'Placa',
+      width: 150
+    },
+    {
+      field: 'selling_price',
+      headerName: 'Preço de Venda',
+      width: 150
+    },
+    {
+      field: 'selling_date',
+      headerName: 'Data de Venda',
+      width: 150
+    },
+  ]; 
+
+  const [cars, setcars] = React.useState([])
+
+  async function loadData() {
+    try {
+      // Conectamos ao servidor remoto e esperamos uma resposta
+      const response = await fetch('https://api.faustocintra.com.br/v2/cars')
+      // Extraímos da resposta os dados em formato JSON
+      const data = await response.json()
+      // Armazenamos os dados na variável de estado
+      setcars(data)
+    }
+    catch(error) {
+      // Exibimos o erro no console, para efeitos de depuração
+      console.error(error)
+      // Informamos o erro ao usuário
+      alert('ERRO: ' + error.message)
+    }
+  }
+
+  React.useEffect(() => {
+    loadData()
+  }, []) 
+
+  return <>
+    <Typography variant="h1" gutterBottom>
+      Listagem de veículos
+    </Typography>
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={cars}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        renderCell
+      />
+    </Box>
+  </>
 }
