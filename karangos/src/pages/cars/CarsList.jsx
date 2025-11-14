30a8942a0f879495754a5d11442ad58d9904102d
André <andrecarrijo0710@gmail.com>
2025-11-14 08:02:34 -0300

    14/11 - Entrega do Trabalho 2

--- karangos/src/pages/cars/CarsList.jsx
+++ karangos/src/pages/cars/CarsList.jsx
@@ -1,11 +1,114 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
 
 
 export default function CarsList() {
+
+  const columns = [
+   {
+     field: 'id',
+     headerName: 'Cód.',
+     width: 90
+   },
+   {
+     field: 'brand',
+     headerName: 'Marca e Modelo',
+     width: 250,
+     renderCell: (params) => {
+       return `${params.row.brand} ${params.row.model}`
+     }
+   },
+   {
+     field: 'color',
+     headerName: 'Cor',
+     width: 150
+   },
+   {
+     field: 'year_manufacture',
+     headerName: 'Ano de Fabricação',
+     width: 150
+   },
+   {
+     field: 'imported',
+     headerName: 'Importado',
+     width: 120,
+     renderCell: (params) => {
+       return params.row.imported === 1 ? 'SIM' : ''
+     }
+   },
+   {
+     field: 'plates',
+     headerName: 'Placa',
+     width: 120
+   },
+   {
+     field: 'selling_price',
+     headerName: 'Preço de Venda',
+     width: 150,
+     renderCell: (params) => {
+       if(params.row.selling_price) {
+         return params.row.selling_price.toLocaleString('pt-BR', {
+           style: 'currency',
+           currency: 'BRL'
+         })
+       }
+       return ''
+     }
+   },
+   {
+     field: 'selling_date',
+     headerName: 'Data de Venda',
+     width: 150,
+     valueFormatter: (value) => {
+       if(value) {
+         const date = new Date(value)
+         return date.toLocaleDateString('pt-BR')
+       }
+       return ''
+     }
+   }
+ ];
+
+   const [cars, setCars] = React.useState([])
+
+    async function loadData() {
+   try {
+     const response = await fetch('https://api.faustocintra.com.br/v2/cars')
+     const data = await response.json()
+     setCars(data)
+   }
+   catch(error) {
+     console.error(error)
+     alert('ERRO: ' + error.message)
+   }
+ }
+
+ React.useEffect(() => {
+   loadData()
+ }, [])
+
  return <>
-   <Typography variant="h1">
-     Listagem de veículos
+   <Typography variant="h1" gutterBottom>
+     Listagem de Carros
    </Typography>
+   <Box sx={{ height: 400, width: '100%' }}>
+     <DataGrid
+       rows={cars}
+       columns={columns}
+       initialState={{
+         pagination: {
+           paginationModel: {
+             pageSize: 5,
+           },
+         },
+       }}
+       pageSizeOptions={[5]}
+       checkboxSelection
+       disableRowSelectionOnClick
+     />
+   </Box>
+   
  </>
 }
